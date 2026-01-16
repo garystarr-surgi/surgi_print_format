3ed556dbfba2c94881a7e54b3b2c7a886cd30aeb
garystarr-surgi <gary.starr@surgishop.com>
2026-01-16 11:23:35 -0500

    Add application metadata to hooks.py

--- surgi_print_format/hooks.py
+++ surgi_print_format/hooks.py
@@ -1,3 +1,10 @@
+app_name = "surgi_print_format"
+app_title = "Surgi Print Format"
+app_publisher = "SurgiShop"
+app_description = "Help transfer custom print formats"
+app_email = "gary.starr@surgishop.com"
+app_license = "MIT"
+
 fixtures = [
     {
         "dt": "Print Format",
